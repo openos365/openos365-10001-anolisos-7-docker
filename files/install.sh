966bc99d09351420472637d7f8f1abd45849a73a
gnuhub <gnuhub@gmail.com>
2023-09-20 02:00:06 +0800

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -45,6 +45,7 @@ dnf --assumeyes install dosfstools
 dnf --assumeyes install git
 dnf --assumeyes install expect
 dnf --assumeyes install vim
+dnf --assumeyes groupinstall "Development Tools"
 
 dnf --assumeyes update
 dnf clean all
