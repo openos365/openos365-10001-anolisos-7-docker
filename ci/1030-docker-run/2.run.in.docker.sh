5e6579fa972929142d372c4c7aaacab71fd5ab03
gnuhub <gnuhub@gmail.com>
2023-09-23 11:31:41 +0800

    (just fix it)

--- ci/1030-docker-run/2.run.in.docker.sh
+++ ci/1030-docker-run/2.run.in.docker.sh
@@ -24,10 +24,34 @@ if [ -d versions ];then
 fi
 mkdir versions
 cd versions
-dnf list installed > dnf.list.installed.txt
-dnf list > dnf.list.txt
 
-sed -i '1,2d' dnf.list.installed.txt
-sed -i '1d' dnf.list.txt
+function dnf_list()
+{
+
+    dnf list installed > dnf.list.installed.txt
+    dnf list > dnf.list.txt
+
+    sed -i '1,2d' dnf.list.installed.txt
+    sed -i '1d' dnf.list.txt
+}
+
+function apt_list()
+{
+
+    apt list --installed > apt.list.installed.txt
+    apt list > apt.list.txt
+
+    sed -i '1,2d' apt.list.installed.txt
+    sed -i '1d' apt.list.txt
+}
+which dnf
+if [ $? -eq 0 ];then
+    dnf_list
+fi
+
+which apt
+if [ $? -eq 0 ];then
+    apt_list
+fi
 
 echo "============================================================================"
